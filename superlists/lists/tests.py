0a769ea7020fc3f0727b7a5e51e786ca08941fdf
Jef Neefs <neefsj@gmail.com>
2018-08-28 23:23:11 +0200

    refactired tge furst time

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,4 +1,5 @@
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from django.urls import resolve
 from django.test import TestCase
 from lists.views import home_page
@@ -10,9 +11,5 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_htmt(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -6,4 +6,4 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
