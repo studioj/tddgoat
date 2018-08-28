0ba0ceb160d06203cdf725e3f4e88c055e8dda07
Jef Neefs <neefsj@gmail.com>
2018-08-28 22:02:55 +0200

    titel in orde

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,9 +1,18 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+from django.http import HttpRequest
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_htmt(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
