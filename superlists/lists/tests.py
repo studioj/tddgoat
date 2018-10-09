0d0748ebf79f973f39f7a3945aa3866be532b6d5
Jef Neefs <neefsj@gmail.com>
2018-10-09 23:19:14 +0200

    expanded functional test 64

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,3 @@
-from django.http import HttpRequest
-from django.template.loader import render_to_string
 from django.urls import resolve
 from django.test import TestCase
 from lists.views import home_page
@@ -12,4 +10,9 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_htmt(self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
