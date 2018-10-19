233e8beee76064eb75bedd8c6399aaa88d33a120
Jef Neefs <neefsj@gmail.com>
2018-10-20 00:45:46 +0200

    p80

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from lists.models import Item
 
 
 def home_page(request):
-    return render(request, 'home.html', {"new_item_text": request.POST.get('item_text', '')})
+    if request.method == 'POST':
+        new_item = request.POST['item_text']
+        Item.objects.create(text=new_item)
+        return redirect('/')
+    items = Item.objects.all()
+    a = Item.objects.first()
+    return render(request, 'home.html', {'items': items})
+
