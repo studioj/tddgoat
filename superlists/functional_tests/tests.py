4a316881ccd292b80147cac40376b0ba816dcec7
studioj <neefsj@gmail.com>
2020-04-24 01:48:49 +0200

    making cls methods

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -31,6 +31,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
         options.headless = True
         self.browser = webdriver.Firefox(options=options)
 
+    def tearDown(self):
+        self.browser.quit()
+
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
