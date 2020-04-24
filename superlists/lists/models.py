bffd5b0fca640c3e696ae3ca8ee72e6053a61a97
studioj <neefsj@gmail.com>
2020-04-24 02:23:55 +0200

    making cls methods

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -10,4 +10,4 @@ class List(models.Model):
 
 class Item(models.Model):
     text = models.TextField(default='')
-    list = models.ForeignKey(List, default=None, on_delete=None)
+    list = models.ForeignKey(List, default=None)
